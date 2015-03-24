d5922ffd0ac857467150ea3df94bf66c772ec4ee
python490 <python490@gmail.com>
2015-03-23 20:20:10 -0700

    logic stuff playing around

---
+++ ooz.py
@@ -0,0 +1,16 @@
+from logic import *
+
+
+print dpll_satisfiable(A  | B)
+
+p = 0.2
+
+def generate():
+    return random.random() <= p
+
+print generate()
+print generate()
+print generate()
+print generate()
+print generate()
+print generate()
